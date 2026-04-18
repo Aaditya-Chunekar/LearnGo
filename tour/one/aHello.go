127ce0a5106383be889b25f0e886e8a0d8a23b88
Aaditya-Chunekar <aadityachunekar@gmail.com>
2026-04-18 12:48:34 +0530

    [tour] module one

---
+++ tour/one/aHello.go
@@ -0,0 +1,5 @@
+package main
+
+import "fmt"
+//pkg and func name should be main
+func main() { fmt.Println("Hello, Aaditya") }
---
+++ tour/one/bImportTime.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+// group imports, no commas, packages in double quotes
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+	// naming convention is Pascal case
+	fmt.Println("The time is", time.Now())
+	// No semicolons - required only for one-liners - similar to py
+}
